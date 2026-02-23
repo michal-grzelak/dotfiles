5f85c7a07a1a46dd9e1b8ae3dd412f7b8f23fc2c
Michał Grzelak <grzelakm@outlook.com>
2026-02-23 19:41:35 +0100

    chore: fix init

--- home/dot_config/bash/config-loader.sh
+++ home/dot_config/bash/config-loader.sh
@@ -1,26 +1,13 @@
 # MANAGED BY CHEZMOI! EDIT ONLY WITH: chezmoi edit
 
-# Securely load all .sh files from conf.d and subdirectories in sorted order
+# Securely load all .sh files from conf.d in sorted order
 # Only source files owned by the current user for security
 
 _confd_dir="$HOME/.config/bash/conf.d"
 
 if [[ -d "$_confd_dir" ]]; then
-    _owned_files=()
-    while IFS= read -r -d '' _file; do
-        if [[ -O "$_file" ]]; then
-            _owned_files+=("$_file")
-        fi
-    done < <(find "$_confd_dir" -type f -name '*.sh' -print0)
-
-    _sorted_files=()
-    while IFS= read -r -d '' _file; do
-        _sorted_files+=("$_file")
-    done < <(printf '%s\0' "${_owned_files[@]}" | sort -z)
-
-    for _file in "${_sorted_files[@]}"; do
-        source "$_file"
+    for _file in "$_confd_dir"/*.sh; do
+        [[ -f "$_file" && -O "$_file" ]] && source "$_file"
     done
-
-    unset _confd_dir _owned_files _sorted_files _file
+    unset _confd_dir _file
 fi
