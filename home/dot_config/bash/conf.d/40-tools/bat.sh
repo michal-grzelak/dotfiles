3d99b8cbff32e9d09ff85c31589c32a2b1f2cf92
Michał Grzelak <grzelakm@outlook.com>
2026-02-23 19:59:09 +0100

    fix: updat bat config

--- home/dot_config/bash/conf.d/40-tools/bat.sh
+++ home/dot_config/bash/conf.d/40-tools/bat.sh
@@ -1,4 +1,7 @@
 # MANAGED BY CHEZMOI! EDIT ONLY WITH: chezmoi edit
 
+# Bat config path
+export BAT_CONFIG_PATH="${XDG_CONFIG_HOME}/bat/config.conf"
+
 # Bat alias (cat replacement)
 alias cat='bat --paging=never'
