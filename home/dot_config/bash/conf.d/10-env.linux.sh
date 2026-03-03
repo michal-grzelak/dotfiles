6ccc2a4692d8a371501f5f83231c781d9c104792
Michał Grzelak <grzelakm@outlook.com>
2026-03-03 20:40:42 +0100

    fix: repair 1pass ssh integration

--- home/dot_config/bash/conf.d/10-env.linux.sh
+++ home/dot_config/bash/conf.d/10-env.linux.sh
@@ -1,3 +1,6 @@
 # Linux configuration
 
 export GPG_TTY=$(tty)
+
+# Forward ssh to 1Pass socket
+export SSH_AUTH_SOCK=~/.1password/agent.sock
--- home/dot_config/fish/conf.d/10-env.linux.fish
+++ home/dot_config/fish/conf.d/10-env.linux.fish
@@ -1,3 +1,6 @@
 # Linux configuration
 
 set -gx GPG_TTY (tty)
+
+# Forward ssh to 1Pass socket
+set -gx SSH_AUTH_SOCK ~/.1password/agent.sock
