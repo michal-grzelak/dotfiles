09cd1c7297360490b0130023446eebcfc8a9cdc5
Michal Grzelak <grzelakm@outlook.com>
2026-03-13 08:35:19 +0100

    feat: override docker with podman

--- home/dot_config/fish/conf.d/10-env.common.fish
+++ home/dot_config/fish/conf.d/10-env.common.fish
@@ -12,3 +12,6 @@ set -gx PAGER delta
 set -gx XDG_CONFIG_HOME ~/.config
 set -gx XDG_DATA_HOME ~/.local/share
 set -gx XDG_CACHE_HOME ~/.cache
+
+# override docker with podman
+set -gx DOCKER_HOST "unix://$XDG_RUNTIME_DIR/podman/podman.sock"
